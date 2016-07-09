302f709738219f649cd17303bf5102e41dfed4b0
KiruthikaRajasekaran <krithikasadhana213@gmail.com>
2016-07-09 12:03:38 +0530

    finish

---
+++ README.c
@@ -0,0 +1,24 @@
+# largesr
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("enter the value of a");
+printf("enter the value of b");
+printf("enter the value od c");
+if(a>b&&a>c)
+{
+printf("a is larger");
+}
+if(b>c)
+{
+printf("b is larger"):
+}
+else
+{
+printf("c is larger");
+}
+getch();
+}
